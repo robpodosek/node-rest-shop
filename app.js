ed1f776bc1176c4da5080df08af3f69cf25da053
Robert Podosek <rpodosek@gmail.com>
2022-06-21 16:25:31 -0700

    Add error handling and logging

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
+app.use(morgan("dev"));
+
+// Routes that handle requests
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error("Not found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+  });
+});
 
 module.exports = app;
