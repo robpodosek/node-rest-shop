fb3225423911d6d28c5db9bb3951154b01010d4a
Robert Podosek <rpodosek@gmail.com>
2022-06-22 13:38:51 -0700

    Setup mongodb. Create product model and update products api resource.

---
+++ api/models/product.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const productSchema = mongoose.Schema({
+  _id: mongoose.SchemaTypes.ObjectId,
+  name: String,
+  price: Number,
+});
+
+module.exports = mongoose.model("Product", productSchema);
--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Connect to Mongo Atlas
+const db =
+  "mongodb+srv://node-rest-shop:" +
+  process.env.MONGO_ATLAS_PW +
+  "@node-rest-shop.ubxj8.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 // Routes that handle requests
 const productRoutes = require("./api/routes/products");
