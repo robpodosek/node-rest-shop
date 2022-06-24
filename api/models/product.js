5a4211ac2ab3182662d17fb3af9f39165045c403
Robert Podosek <rpodosek@gmail.com>
2022-06-24 14:54:36 -0700

    Handle orders

--- api/models/product.js
+++ api/models/product.js
@@ -1,8 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
 const productSchema = mongoose.Schema({
-  _id: mongoose.SchemaTypes.ObjectId,
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
 });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model('Product', productSchema);
